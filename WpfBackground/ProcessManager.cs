1ac9ae1df6cd8080b13a90daa14361a16a9e1979
kenichi_shintome <kshintome@say-tech.co.jp>
2018-08-15 14:26:27 +0900

    非同期タスクの確認

---
+++ WpfBackground/ProcessManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WpfBackground
+{
+    public class ProcessManager
+    {
+        public void StartProcessAsync(CancellationTokenSource cts)
+        {
+            var psi = new ProcessStartInfo("cmd.exe", "/c ping 127.0.0.1 -t");
+            //            var psi = new ProcessStartInfo("cmd.exe", "/c ping 127.0.0.1 -n 5");
+            //                        var psi = new ProcessStartInfo(@"E:\develop\Cloud Storage Mount\_DEBUG\bin\s3mount.exe", @"mount --common ""C:\Users\kenichi_shintome.SWAN\AppData\Local\SAY Technologies\Cloud Storage Drive Mount Tools\common.json""  --account ""C:\Users\kenichi_shintome.SWAN\AppData\Local\SAY Technologies\Cloud Storage Drive Mount Tools\accounts.json"" --setting ""C:\Users\kenichi_shintome.SWAN\AppData\Local\SAY Technologies\Cloud Storage Drive Mount Tools\drives\f1b70724-797f-4af2-8255-1e0bb92988c1.json""");
+            //var psi = new ProcessStartInfo(@"E:\develop\Cloud Storage Mount\_DEBUG\bin\s3mount.exe", @"mount --common ""C:\Users\kenichi_shintome.SWAN\AppData\Local\SAY Technologies\Cloud Storage Drive Mount Tools\common.json""  --account ""C:\Users\kenichi_shintome.SWAN\AppData\Local\SAY Technologies\Cloud Storage Drive Mount Tools\accounts.json"" --setting ""C:\Users\kenichi_shintome.SWAN\AppData\Local\SAY Technologies\Cloud Storage Drive Mount Tools\drives\f1b70724-797f-4af2-8255-1e0bb92988c1.json""");
+
+//            Action<Process> callBackProcess = (p) => { Debug.WriteLine(@"プロセスアクションが呼ばれました。"); };
+            Action<string> output = (string s) => Debug.WriteLine(s);
+
+            psi.StartAsync(output, output, null, cts.Token).ConfigureAwait(false);
+            Debug.WriteLine($"psi.StartAsyncを呼び出しました。");
+        }
+    }
+}
--- WpfBackground/ViewModels/MainWindowViewModel.cs
+++ WpfBackground/ViewModels/MainWindowViewModel.cs
@@ -1,19 +1,73 @@
-﻿using Prism.Mvvm;
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+using Reactive.Bindings;
+
 
 namespace WpfBackground.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        private string _title = "Prism Application";
-        public string Title
-        {
-            get { return _title; }
-            set { SetProperty(ref _title, value); }
-        }
+        public ReactiveProperty<string> Title { get; } = new ReactiveProperty<string>("Prism Application");
+
+        public ReactiveCommand StartCommand { get; }
+
+        public ReactiveCommand CancelCommand { get; }
+
+        private ProcessManager processManager = new ProcessManager();
+
+        private CancellationTokenSource cts = new CancellationTokenSource();
+
 
         public MainWindowViewModel()
         {
 
+            this.StartCommand = new ReactiveCommand();
+            this.StartCommand.Subscribe(_ =>
+            {
+                Debug.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}]ボタン実行開始します。");
+                StartAsync().ContinueWith(task => { Debug.WriteLine("タスクが完了しました。"); }); ;
+//                Start();
+                Debug.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}]ボタン実行完了しました。");
+            });
+
+            //this.StartCommand.Subscribe(_ =>
+            //{
+            //    processManager.StartProcessAsync(cts);
+            //});
+
+
+            this.CancelCommand = new ReactiveCommand();
+            //this.CancelCommand.Subscribe(_ =>
+            //{
+            //    cts.Cancel();
+            //});
         }
+
+        private async Task StartAsync()
+        {
+            Debug.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}]関数開始します。");
+            var client = new WebClient();
+            await client.DownloadDataTaskAsync(@"https://www.google.com/")
+                .ContinueWith(task =>
+                {
+                    Debug.WriteLine(Encoding.UTF8.GetString(task.Result));
+                });
+            Debug.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}]関数から抜けます");
+        }
+
+        private void Start()
+        {
+            Debug.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}]関数開始します。");
+            var client = new WebClient();
+            var data = client.DownloadData(@"https://www.google.com/");
+            Debug.WriteLine(Encoding.UTF8.GetString(data));
+            Debug.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}]関数から抜けます");
+        }
+
     }
 }
